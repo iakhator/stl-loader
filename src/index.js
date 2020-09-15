0dce9cf9e67a53cc787b046932f81ea062febe6c
Akhator Itua <iakhator@gmail.com>
2020-09-15 08:21:31 +0100

    add stl loader

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log('Hello World')
+import * as THREE from 'three';
+import { STLLoader } from 'three/examples/jsm/loaders/STLLoader.js';
+
+(() => {
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  const renderer = new THREE.WebGLRenderer();
+
+  renderer.setSize( window.innerWidth, window.innerHeight);
+  document.body.appendChild( renderer.domElement ); 
+  
+  // const geometry = new THREE.BoxGeometry();
+  // const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  // const cube = new THREE.Mesh( geometry, material );
+  // scene.add( cube );
+
+  const loader = new STLLoader();
+  console.log(loader.load(), 'mesh')
+  loader.load( '../models/stl/marketing_concept_v18.stl', function ( geometry ) {
+    var material = new THREE.MeshLambertMaterial( { color: 0xFFFFFF, specular: 0x111111, shininess: 200 } );
+    var mesh = new THREE.Mesh( geometry, material );
+    mesh.position.set( 0, 20, 0);
+    scene.add( mesh );
+  });
+  
+  camera.position.z = 5;
+
+  // function animate() {
+  //   requestAnimationFrame( animate );
+  //   cube.rotation.x += 0.01;
+  //   cube.rotation.y += 0.01;
+  //   cube.rotation.z += 0.01
+  //   renderer.render( scene, camera );
+  // }
+
+  // animate()
+})()
